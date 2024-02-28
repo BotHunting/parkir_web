20e3a1f8c0b6830c0a1b52c0c5a5c81c34265d01
Bot Hunting <82310684+BotHunting@users.noreply.github.com>
2024-02-28 21:29:30 +0900

    navbar.php

--- navbar.php
+++ navbar.php
@@ -37,4 +37,5 @@
         <a href="laporan.php">Laporan</a>
     </nav>
 </body>
+    <script type='text/javascript' src='https://assets.trakteer.id/js/trbtn-overlay.min.js'></script><script type='text/javascript' class='troverlay'>(function() {var trbtnId = trbtnOverlay.init('Support Me','#FFC147','https://trakteer.id/hunty/tip/embed/modal','https://cdn.trakteer.id/images/embed/trbtn-icon.png?date=18-11-2023','35','floating-right');trbtnOverlay.draw(trbtnId);})();</script>
 </html>
